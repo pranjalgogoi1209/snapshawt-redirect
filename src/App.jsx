9a125e06a0803997e1538f0857f6ba0695f3fe83
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-05-22 18:08:56 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React, { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     // Redirect to the specified URL
-    window.location.href = "https://snapshawt.com";
+    window.location.href =
+      "https://snapshawt.com/face-swap/single-ai-face-swap";
   }, []);
   return <div></div>;
 }
